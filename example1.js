af364e9cac95e0bdc5ff91322dcc0c291d6378a6
se-hyun Hwang <imscs21@naver.com>
2014-02-23 15:25:00 +0900

    Update example1.js

--- example1.js
+++ example1.js
@@ -1,12 +1,12 @@
-var result="Your external ip address";
+var result='Your external ip address';
 var language = window.navigator.userLanguage || window.navigator.language;
 if(language == "ko-KR"){
-  result="당신의 공인 아이피";
+  result='당신의 공인 아이피';
 }
 else{
- result="Your external ip address";
+ result='Your external ip address';
 }
 
 
-document.write(result+"a");
-document.write("<br>");
+document.write(result);
+document.write('<br>');
