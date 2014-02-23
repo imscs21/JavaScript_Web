bdc0a69102d985252f261dde010eba5615c55c50
se-hyun Hwang <imscs21@naver.com>
2014-02-23 11:12:08 +0900

    update8

--- example1.js
+++ example1.js
@@ -1,14 +1,15 @@
 //(function(){
 var result="Your external ip address: ";
 var language = window.navigator.userLanguage || window.navigator.language;
-if(language = "ko-KR"){
+if(language == "ko-KR"){
   result="당신의 공인 아이피: ";
 }
 else{
  result="Your external ip address: ";
 }
 
-//document.write(result);
+//
+document.write(result);
 //}
 //)();
 //checklanguage();
